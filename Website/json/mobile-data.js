3540ee7e2484cea3427f6a0235eb34958e8465c4
m7amad45 <m7amad45@gmail.com>
2023-03-30 06:23:57 +0300

    phas3 1st task

---
+++ Website/json/mobile-data.js
@@ -0,0 +1,34 @@
+var mobiles = [
+    {
+
+        title: "iPhone 13 Pro",
+        desc: "Smooth, responsive Super Retina XDR display with ProMotion. Huge camera system upgrade for dramatic new possibilities. Exceptional durability. The ultrafast A15 Bionic chip. And breakthrough battery life. Let’s Pro.",
+        price: "5199 SAR"
+
+    },
+    {
+
+        title: "ASUS ROG NVIDIA GEFORCE RTX 3090 GRAPHIC CARD",
+        desc: "24 GB GDDR6X, 1.73 GHZ BOOST CLOCK, ROG-STRIX-RTX3090-24G-GAMING, 2X HDMI, 384 BIT BUS WIDTH, 3 X DISPLAYPORT, HDMI.",
+        price: "14000 SAR"
+    },
+    {
+
+        title: "Samsung Galaxy S21 Ultra",
+        desc: "Introducing Galaxy S21 Ultra 5G. Designed with a unique contour-cut camera to create a revolution in photography — letting you capture cinematic 8K video and snap epic stills, all in one go. And with Galaxy's fastest chip, strongest glass, 5G and an all-day battery, Ultra easily lives up to its name.",
+        price: "4299 SAR"
+    },
+    {
+
+        title: "BenQ ZOWIE XL2546K",
+        desc: "24.5inch 240Hz Esports Gaming Monitor,0.5ms, FHD| Height Adjustable| FreeSync Premium | DP, HDMI| DyAc+ for Recoil Control, Black eQualizer & Color Vibrance| S-Switch for Game Mode.",
+        price: "2100 SAR"
+    },
+    {
+
+        title: "Logitech G PRO X SUPERLIGHT",
+        desc: "Wireless Gaming Mouse, HERO 25K Sensor, Ultra-light with 63g, 5 Programmable Buttons, 70 hours Battery Life, Zero Additive PTFE Feet, PC/Mac.",
+        price: "700 SAR"
+    }
+
+]
